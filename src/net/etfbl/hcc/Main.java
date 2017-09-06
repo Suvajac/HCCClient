a0155354c9967d0a4448d530f9cbbaf0b2a2dc53
lekic <lekic@DESKTOP-K3L81EV>
2017-09-06 12:57:42 +0200

    Packet reorganization 3

--- src/net/etfbl/hcc/Main.java
+++ src/net/etfbl/hcc/Main.java
@@ -1,12 +1,11 @@
 package net.etfbl.hcc;
 
-import net.etfbl.hcc.view.LoginController;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import net.etfbl.hcc.controller.LoginController;
 
 
 public class Main extends Application {
@@ -25,9 +24,7 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
-    	System.out.println("KlassPath");
         launch(args);
     }
 }
