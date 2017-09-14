17c5f5333b040bd8d78558e8717a0663396c9431
Bojan <boki94@msn.com>
2017-09-14 17:22:44 +0200

    Dodan je alert kod narucivanja usluga

---
+++ src/net/etfbl/hcc/view/gost/PotvrdaAlertController.java
@@ -0,0 +1,74 @@
+package net.etfbl.hcc.view.gost;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import net.etfbl.hcc.Client;
+import net.etfbl.hcc.model.Usluga;
+
+public class PotvrdaAlertController implements Initializable{
+	@FXML
+	private Label potvrdaLabel;
+	@FXML
+	private Label tekstLabel;
+	@FXML
+	private Label statusLabel;
+	@FXML
+	private Button daButton;
+	@FXML
+	private Button neButton;
+	
+	private StackPane stackPane;
+	private AnchorPane anchorPane;
+	private Usluga usluga;
+	private ResourceBundle rb;
+	
+	public void initialize(URL url,ResourceBundle rb){
+		this.rb = rb;
+	}
+	
+	public void setTekstLabel(){
+		tekstLabel.setText(rb.getString("daLiZelite")+" "+usluga.getNaziv()+" "+rb.getString("poCijeni")+" "+usluga.getCijena()+" EUR?");
+	}
+	
+	@FXML
+	public void handleDa(){
+		int idUsluge = Client.getInstance().dodajUslugu(usluga,RootGostController.gost.getRacun());
+		if(idUsluge>0){
+			statusLabel.setText(rb.getString("uspjesno"));
+		}
+		daButton.setDisable(true);
+	}
+	
+	@FXML
+	public void handleNe(){
+		anchorPane.toBack();
+		stackPane.getChildren().remove(anchorPane);
+	}
+	
+	public void setStackPane(StackPane stackPane) {
+		this.stackPane = stackPane;
+	}
+
+	public void setAnchorPane(AnchorPane anchorPane) {
+		this.anchorPane = anchorPane;
+	}
+
+	public Usluga getUsluga() {
+		return usluga;
+	}
+
+	public void setUsluga(Usluga usluga) {
+		this.usluga = usluga;
+		setTekstLabel();
+	}
+
+	
+	
+}
